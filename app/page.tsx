edb9f51848f9129120ce333a071a7ec31f350204
Daniel Santos <danielfilipe.df@gmail.com>
2025-03-29 12:41:18 -0300

    fix: adiciona página raiz

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+// satyz-site/app/page.tsx
+export default function Home() {
+    return (
+      <div>
+        <h1>Satyz está no ar!</h1>
+        <p>Landing em construção 🚀</p>
+      </div>
+    );
+  }
