3373076fbf829fd5cba25fd69df46f1be5b56243
Daniel Santos <danielfilipe.df@gmail.com>
2025-03-30 13:17:34 -0300

    feat: atualiza AIWorkers com vídeos responsivos e animações

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -111,17 +111,19 @@ const Header = () => {
 
           <div className="mt-7 flex items-center gap-6 xl:mt-0">
             <ThemeToggler />
+
             <Link
-              href="https://github.com/NextJSTemplates/solid-nextjs"
+              href="/auth/signin"
               className="text-regular font-medium text-waterloo hover:text-primary"
             >
-              GitHub Repo 🌟
+              Log in
             </Link>
+
             <Link
-              href="https://nextjstemplates.com/templates/solid"
+              href="/auth/signup"
               className="flex items-center justify-center rounded-full bg-primary px-7.5 py-2.5 text-regular text-white duration-300 ease-in-out hover:bg-primaryho"
             >
-              Get Pro 🔥
+              Get Started 🚀
             </Link>
           </div>
         </div>
