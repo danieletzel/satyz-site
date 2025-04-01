0b6775c71e76af556fc995c96c9f5f4e295630ae
Daniel Santos <danielfilipe.df@gmail.com>
2025-04-01 19:18:18 -0300

    feat: substitui FunFact por FeaturesCarousel com spotlight animado

--- components/Blog/BlogItem.tsx
+++ components/Blog/BlogItem.tsx
@@ -3,12 +3,18 @@
 import { Blog } from "@/types/blog";
 import Image from "next/image";
 import Link from "next/link";
+import { motion } from "framer-motion";
 
 const BlogItem = ({ blog }: { blog: Blog }) => {
   const { mainImage, title, metadata } = blog;
 
   return (
-    <div className="group rounded-lg bg-white p-4 pb-9 shadow-solid-8 transition-all duration-300 hover:-translate-y-1 hover:shadow-xl dark:bg-blacksection">
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.4 }}
+      className="group rounded-lg bg-white p-4 pb-9 shadow-solid-8 transition-all duration-300 hover:-translate-y-1 hover:shadow-xl dark:bg-blacksection"
+    >
       <Link
         href="/blog/blog-details"
         className="relative block aspect-[368/239] overflow-hidden rounded-md"
@@ -34,7 +40,7 @@ const BlogItem = ({ blog }: { blog: Blog }) => {
           {metadata}
         </p>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
