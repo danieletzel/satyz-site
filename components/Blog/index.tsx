8d5071bc38db26e66447ea08d227985d4b75f047
Daniel Santos <danielfilipe.df@gmail.com>
2025-04-01 19:31:22 -0300

    feat: substitui FunFact por FeaturesCarousel com spotlight animado

--- components/Blog/index.tsx
+++ components/Blog/index.tsx
@@ -7,7 +7,7 @@ import BlogData from "./blogData";
 
 const Blog = () => {
   return (
-    <section className="py-20 lg:py-25 xl:py-30">
+    <section id="blog" className="py-20 lg:py-25 xl:py-30">
       <div className="mx-auto max-w-c-1315 px-4 md:px-8 xl:px-0">
         <div className="animate_top mx-auto text-center">
           <SectionHeader
@@ -23,8 +23,8 @@ const Blog = () => {
 
       <div className="mx-auto mt-15 max-w-c-1280 px-4 md:px-8 xl:mt-20 xl:px-0">
         <div className="grid grid-cols-1 gap-7.5 md:grid-cols-2 lg:grid-cols-3 xl:gap-10">
-          {BlogData.slice(0, 3).map((blog, key) => (
-            <BlogItem blog={blog} key={key} />
+          {BlogData.slice(0, 3).map((blog) => (
+            <BlogItem blog={blog} key={blog._id} />
           ))}
         </div>
       </div>
