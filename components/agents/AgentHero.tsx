c57d976c0ff803b7e2b6a66c66ba9eac1e8021b6
Daniel Santos <danielfilipe.df@gmail.com>
2025-04-02 15:17:37 -0300

    feat: substitui FunFact por FeaturesCarousel com spotlight animado

--- components/agents/AgentHero.tsx
+++ components/agents/AgentHero.tsx
@@ -3,7 +3,7 @@
 
 import { motion } from "framer-motion";
 import { Typewriter } from "react-simple-typewriter";
-import ThemeToggler from "../ThemeToggler";
+import ThemeToggler from '@/components/ThemeToggler';
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
