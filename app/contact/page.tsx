bcc529b83eef50d4c7044603fcb089647d10f47d
Daniel Santos <danielfilipe.df@gmail.com>
2025-04-03 10:29:27 -0300

    feat: substitui FunFact por FeaturesCarousel com spotlight animado

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,7 +2,7 @@
 
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import Contact from "@/components/contact/index";
+import Contact from "@/components/Contact";
 
 export default function ContactPage() {
   return (
